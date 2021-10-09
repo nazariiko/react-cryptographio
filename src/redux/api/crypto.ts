154b3b7e78a31cae3bfa01475fbf9bd4fb0087e0
Nazarii Kovalenko <nazarii.dev@gmail.com>
2021-10-09 03:11:05 +0300

    first commit

--- src/redux/api/crypto.ts
+++ src/redux/api/crypto.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const URL = 'https://api.nomics.com/v1/';
-const API_KEY = 'c49d04d2873f612dfc1eaeee24dc21e97dee6af3'; 
+const API_KEY = 'c49d04d2873f612dfc1eaeee24dc21e97dee6af3';
 
 export const cryptoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: URL }),
@@ -10,10 +10,11 @@ export const cryptoApi = createApi({
     getCryptocurrencies: builder.query({
       query: (perPage) => ({
         method: 'GET',
+        mode: 'no-cors',
         url: `${URL}currencies/ticker?key=${API_KEY}&page=1&per-page=${perPage}&interval=1d,7d`,
       }),
-    })
-  })
-})
+    }),
+  }),
+});
 
-export const { useGetCryptocurrenciesQuery } = cryptoApi
+export const { useGetCryptocurrenciesQuery } = cryptoApi;
